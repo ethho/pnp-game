aed04306e182b4368c85cb3cef3439d9dfac8b57
Ethan Ho <ethan.n.ho@gmail.com>
2023-07-18 16:03:49 -0500

    Add WIP Tiled maps

---
+++ assets/tux_bros_collect.tsx
@@ -0,0 +1,161 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tux_bros_collection" tilewidth="24" tileheight="16" tilecount="52" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <transformations hflip="1" vflip="0" rotate="0" preferuntransformed="0"/>
+ <tile id="0">
+  <image width="16" height="16" source="images/block_blackexcl.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="images/block_blackqm.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="images/block_bush0.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="images/block_bush1_bot.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="images/block_bush1_top.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="images/block_color_aqua.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="images/block_color_black.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="images/block_color_blue0.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="images/block_color_blue1.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="images/block_color_blue2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="images/block_color_brown.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="images/block_color_dgrey.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="images/block_color_green0.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="images/block_color_green1.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="images/block_color_lgrey.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="images/block_color_purple0.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="images/block_color_purple1.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="images/block_color_red0.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="images/block_color_red1.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="images/block_color_white.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="images/block_color_yellow0.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="images/block_dirt0_left.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="16" source="images/block_dirt0_mid.png"/>
+ </tile>
+ <tile id="23">
+  <image width="16" height="16" source="images/block_dirt0_right.png"/>
+ </tile>
+ <tile id="24">
+  <image width="16" height="16" source="images/block_dirt0_solo.png"/>
+ </tile>
+ <tile id="25">
+  <image width="16" height="16" source="images/block_dirt1_left.png"/>
+ </tile>
+ <tile id="26">
+  <image width="16" height="16" source="images/block_dirt1_mid.png"/>
+ </tile>
+ <tile id="27">
+  <image width="16" height="16" source="images/block_dirt1_right.png"/>
+ </tile>
+ <tile id="28">
+  <image width="16" height="16" source="images/block_dirt1_solo.png"/>
+ </tile>
+ <tile id="29">
+  <image width="16" height="16" source="images/block_dollar.png"/>
+ </tile>
+ <tile id="30">
+  <image width="16" height="16" source="images/block_earth0_left.png"/>
+ </tile>
+ <tile id="31">
+  <image width="16" height="16" source="images/block_earth0_mid.png"/>
+ </tile>
+ <tile id="32">
+  <image width="16" height="16" source="images/block_earth0_right.png"/>
+ </tile>
+ <tile id="33">
+  <image width="16" height="16" source="images/block_goldplatform.png"/>
+ </tile>
+ <tile id="34">
+  <image width="16" height="16" source="images/block_goldslat.png"/>
+ </tile>
+ <tile id="35">
+  <image width="16" height="16" source="images/block_goldstar.png"/>
+ </tile>
+ <tile id="36">
+  <image width="16" height="16" source="images/block_grass0.png"/>
+ </tile>
+ <tile id="37">
+  <image width="16" height="16" source="images/block_plat_gold_left.png"/>
+ </tile>
+ <tile id="38">
+  <image width="16" height="16" source="images/block_plat_gold_mid.png"/>
+ </tile>
+ <tile id="39">
+  <image width="16" height="16" source="images/block_plat_gold_right.png"/>
+ </tile>
+ <tile id="40">
+  <image width="16" height="16" source="images/block_plat_gold_solo.png"/>
+ </tile>
+ <tile id="41">
+  <image width="16" height="16" source="images/block_star.png"/>
+ </tile>
+ <tile id="42">
+  <image width="16" height="16" source="images/block_whiteexcl.png"/>
+ </tile>
+ <tile id="43">
+  <image width="16" height="16" source="images/block_whiteqm.png"/>
+ </tile>
+ <tile id="44">
+  <image width="16" height="16" source="images/character_pook0_cool.png"/>
+ </tile>
+ <tile id="45">
+  <image width="16" height="16" source="images/character_pook0_idle.png"/>
+ </tile>
+ <tile id="46">
+  <image width="16" height="16" source="images/character_pook0_jump.png"/>
+ </tile>
+ <tile id="47">
+  <image width="24" height="12" source="images/character_pook0_slide0.png"/>
+ </tile>
+ <tile id="48">
+  <image width="16" height="16" source="images/character_pook1_cool.png"/>
+ </tile>
+ <tile id="49">
+  <image width="16" height="16" source="images/character_pook1_idle.png"/>
+ </tile>
+ <tile id="50">
+  <image width="16" height="16" source="images/character_pook1_jump.png"/>
+ </tile>
+ <tile id="51">
+  <image width="24" height="12" source="images/character_pook1_slide0.png"/>
+ </tile>
+</tileset>
